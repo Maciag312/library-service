fc8e770c315d1dbb282f51f8392c3b74e9d69cc0
Bartek Maciag <bartekmaciag@icloud.com>
2021-03-21 18:06:06 +0100

    add spring

---
+++ src/test/kotlin/pwr/library/libraryservice/LibraryServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package pwr.library.libraryservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LibraryServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
